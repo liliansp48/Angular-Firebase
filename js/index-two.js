c02423054b263900043e9a87ddb97b8adf2b9a72
Pereira <lilian.pereira@capgemini.com>
2016-11-05 18:15:29 -0200

    implementando o insert

--- js/index-two.js
+++ js/index-two.js
@@ -1,4 +1,37 @@
+  var bancoDados = new Firebase("https://angular-firebase-a4595.firebaseio.com/angular-firebase");
+
 angular.module('tarefasApp', []).controller('tarefasCtrl',function  ($scope)
 {
-    //Variaveis meu deussssssssssssssssss
+    $scope.tasks = [];
+    $scope.tarefa = "";
+
+    $scope.Adicionar = function(){
+        debugger;
+        bancoDados.push($scope.tarefa)
+    }
+
+    $scope.deletar = function (){
+        $scope.taks.splice(this.$index,1)
+    }
+
+    $scope.inicializacao = function(){
+    }
 });
+
+
+ function ListaTarefas(){
+    debugger;
+    if(bancoDados.length > 0)
+    {
+        for(var i in DB)
+        {
+            $("table > tbody").append( "<tr>"
+                + "<td>"+ DB[i].Id +"</td>"
+                + "<td>"+ DB[i].Nome +"</td>"
+                + "<td>"+ DB[i].Telefone +"</td>"
+                + "<td>"+ DB[i].Email +"</td>"
+                + "</tr>");
+        }
+    }
+}
+
